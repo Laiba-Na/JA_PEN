26e2c918482f0c51bb2faafdf248fee2eb49a922
Laiba Nazir <162683597+Laiba-Na@users.noreply.github.com>
2024-05-31 17:52:35 +0500

    done

--- js/Contact.js
+++ js/Contact.js
@@ -1,11 +1,11 @@
 function sendMail()
 {
         let param ={ 
-            first_name : document.getElementById("FN").value,
-            last_name : document.getElementById("LN").value,
-            email : document.getElementById("EM").value,
-                Phone_no : document.getElementById("PH").value,
-                message : document.getElementById("MS").value,
+            first_name: document.getElementById("first_name").value,
+            last_name: document.getElementById("last_name").value,
+            email: document.getElementById("email").value,
+                Phone_no: document.getElementById("Phone_no").value,
+                message: document.getElementById("message").value,
                 };
 
          emailjs.send("service_bx5ngoo","template_xdjztsc",param).then(alert("Thank you, Your Message has been sent."));
