836d7178de345140c5d58baa5cae313570e0d708
Laiba Nazir <162683597+Laiba-Na@users.noreply.github.com>
2024-05-31 17:39:16 +0500

    emailjs

--- js/Contact.js
+++ js/Contact.js
@@ -3,12 +3,12 @@ function sendMail()
         let param ={ 
             first_name : document.getElementById("FN").value,
             last_name : document.getElementById("LN").value,
-                email : document.getElementById("EM").value,
+            email : document.getElementById("EM").value,
                 Phone_no : document.getElementById("PH").value,
-                message : document.getElementById("PH").value,
+                message : document.getElementById("MS").value,
                 };
 
-         emailjs.send("service_xww2uwl","template_o1d8e74",param).then(alert("Thank you, Your Message has been sent."));
+         emailjs.send("service_bx5ngoo","template_xdjztsc",param).then(alert("Thank you, Your Message has been sent."));
 
 
 }
